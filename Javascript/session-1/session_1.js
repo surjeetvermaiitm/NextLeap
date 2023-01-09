b0e9903cc1c439261f52683452006fd1e7a4a9f1
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-09 23:16:34 +0530

    js

---
+++ Javascript/session-1/session_1.js
@@ -0,0 +1,17 @@
+var a;
+console.log(a); //undefined
+console.log(typeof a); //undifined
+//in JS size doesn't matter
+var a = true; //Boolean
+var b = "Hi";
+var c = 1;
+var d = [1, 2, 3, 4]; //type object
+var f = null; //type object
+console.log(typeof f);
+console.log(f);
+var g = "null"; //String
+var h = {}; //object
+var i = function () {
+  console.log("i");
+};
+console.log(typeof i); //function
