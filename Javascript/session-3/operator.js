0e2ba56cd75289609a908f62a341b566ae974749
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-16 23:44:34 +0530

    JS

--- Javascript/session-3/operator.js
+++ Javascript/session-3/operator.js
@@ -5,3 +5,11 @@
 var d = 1;
 console.log(d++); //1 post incr
 console.log(d); //2
+
+//String Methods: length, toUpperCase, toLowerCase, charAt, substring, slice, indexOf,
+//lastIndexOf, replace, concat, trim, split, join
+// Properties: length
+//Additional Methods: toString, valueOf, concat, repeat, search, match,
+//localeCompare, includes, startsWith, endsWith, padStart, padEnd, trimStart, trimEnd
+
+//temporary dead zone
---
+++ Javascript/session-4/math.js
@@ -0,0 +1,96 @@
+// var a = 10;
+// var b = 20;
+// var c,
+//   d,
+//   e = 30;
+// console.log(a, b, c, d, e);//10 20 undefined undefined 30
+// console.log(Math.PI); //3.141592653589793
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(1, 2));
+// console.log(Math.floor(1.1));
+// console.log(Math.ceil(1.1));
+// console.log(Math.abs(-1.11));
+// console.log(Math.sqrt(6));
+// console.log(Math.pow(2, 3)); //8
+// console.log(Math.max(1, 2, 3));
+// console.log(Math.min(1, 2, 3));
+
+// function checkAdult(age) {
+//   //   if (age > 18) {
+//   //     console.log("adult");
+//   //   } else {
+//   //     console.log("child");
+//   //   }
+//   //   age > 18 ? console.log("adult") : console.log("not adult");
+//   return age >= 18 ? "Major" : "Minor";
+// }
+// console.log(checkAdult(25));
+
+// function checkCategory(item) {
+//   if (item === "Mango") {
+//     return "Fruit";
+//   } else if (item === "Potato") {
+//     return "Veg";
+//   } else {
+//     return "I don't know";
+//   }
+// }
+// console.log(checkCategory("Banana"));
+
+// function checkCategory(item) {
+//   switch (item) {
+//     case "Mango":
+//       console.log("Fruit");
+//       break;
+//     //return something;
+//     case "Potato":
+//       console.log("Veg");
+//       break;
+//     //return;
+//     default:
+//       console.log("I don't know");
+//     //return;
+//   }
+// }
+// checkCategory("Mango");
+
+// function numberToWord(num) {
+//   switch (num) {
+//     case 1: {
+//       return "One";
+//     }
+
+//     case 2: {
+//       return "Two";
+//     }
+
+//     case 3: {
+//       return "Three";
+//     }
+
+//     default: {
+//       return "I don't know";
+//     }
+//   }
+// }
+// console.log(numberToWord(2));
+
+//Array can store any data types
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// for (let index = 1; index <= 10; index++) {
+//   console.log(2 * index);
+// }
+// let i = 1;
+// while (i <= 10) {
+//   console.log(2 * i);
+//   i++;
+// }
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
+  //   i += 2;//or
+}
