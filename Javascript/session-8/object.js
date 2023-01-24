b7e9bc36d0ba2dc8f3e29c3a40328cfc7f5db4c4
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-24 21:36:05 +0530

    js

---
+++ Javascript/session-8/object.js
@@ -0,0 +1,28 @@
+let item = {
+  id: "1",
+  title: "TITLE",
+  description: "DESC",
+  mrp: 100,
+  discountPrice: 80,
+  quantity: 2,
+};
+// item.quantity = 3;
+// console.log(item);
+// for (let k in item) {
+//   console.log(k, item[k]);
+// }
+
+// for (let [k, v] of Object.entries(item)) {
+//   console.log(k, v);
+// }
+// for (let k of Object.keys(item)) {
+//   console.log(k, item[k]);
+// }
+// for (let v of Object.values(item)) {
+//   console.log(v);
+// }
+// console.log(Object.keys(item));
+// console.log(Object.values(item));
+// console.log(Object.entries(item));
+
+// console.table(item);
