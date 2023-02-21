8c82d2669e68764bd75559cf9f8c6ac45fe4750c
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-02-21 11:08:50 +0530

    21-feb

---
+++ DSA/03-2DArray-Matrix/Assignments/01_3_sum/3sum.py
@@ -0,0 +1,26 @@
+#Link: https://leetcode.com/problems/3sum/
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        res=[]
+        n=len(nums)
+        for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            j=i+1
+            k=n-1
+            while j<k:
+                if nums[i]+nums[j]+nums[k]==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    while j<k and nums[j]==nums[j+1]:
+                        j+=1
+                    while k>j and nums[k]==nums[k-1]:
+                        k-=1
+                    j+=1
+                    k-=1
+                elif nums[i]+nums[j]+nums[k]<0:
+                    j+=1
+                else:
+                    k-=1
+                
+        return res
